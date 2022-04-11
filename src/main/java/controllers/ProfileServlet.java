9d807358104a4d0f99297ef23b760c65dab4f151
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-11 08:27:18 -0700

    add profileJsp and ProfileServlet

---
+++ src/main/java/controllers/ProfileServlet.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ProfileServlet", urlPatterns = "/profile")
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+}
