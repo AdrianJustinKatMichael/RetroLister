96640c2446bfb34086758565ba938aeb908fe350
Justin Sebastian <justinthomassebastian@gmail.com>
2022-04-12 13:21:15 -0600

    add util class to get image url

---
+++ src/test/java/TestPosters.java
@@ -0,0 +1,8 @@
+import util.GetPosters;
+
+public class TestPosters {
+    public static void main(String[] args) {
+        String testGameName = "halo";
+        System.out.println("GetPosters.getImagePoster(testGameName) = " + GetPosters.getImagePoster(testGameName));
+    }
+}
