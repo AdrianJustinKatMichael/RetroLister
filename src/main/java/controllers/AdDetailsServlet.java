d25f637a608336cb66b04fde8663ab83a058f0ec
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-04-12 11:14:09 -0500

    uncommented if statement in ad details servlet

--- src/main/java/controllers/AdDetailsServlet.java
+++ src/main/java/controllers/AdDetailsServlet.java
@@ -14,10 +14,10 @@ import java.io.IOException;
 @WebServlet(name = "AdDetailsServlet", urlPatterns = "/ad")
 public class AdDetailsServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        if (request.getSession().getAttribute("user") == null) {
-//            response.sendRedirect("/login");
-//            return;
-//        }
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
         Long adId = Long.valueOf(request.getParameter("id"));
         Ad ad = DaoFactory.getAdsDao().findAdById(adId);
         User creator = DaoFactory.getUsersDao().findUserById(ad.getUserId());
