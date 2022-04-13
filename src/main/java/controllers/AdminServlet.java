7781315e79ba5e55913571459e1ab1d51b151b8d
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-13 09:29:20 -0700

    fix page clear-out bug. admin retains content post input

--- src/main/java/controllers/AdminServlet.java
+++ src/main/java/controllers/AdminServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import dao.DaoFactory;
-import dao.UsersDao;
 import models.User;
 
 import javax.servlet.ServletException;
@@ -14,9 +13,15 @@ import java.io.IOException;
 
 @WebServlet(name = "AdminServlet", urlPatterns = "/admin")
 public class AdminServlet extends HttpServlet {
+
     private HttpSession session;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") != null) {
+            User user = (User) request.getSession().getAttribute("user");
+            String username = user.getUsername();
+            request.setAttribute("username", username);
+        }
         request.setAttribute("users", DaoFactory.getUsersDao().all());
         request.getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
     }
@@ -30,18 +35,18 @@ public class AdminServlet extends HttpServlet {
         if (buttonDel != null) {
             if (confirm.equals("yes")) {
                 DaoFactory.getUsersDao().deleteUser(Long.valueOf(userId));
-                DaoFactory.getUsersDao().all();
+//                request.setAttribute("users", DaoFactory.getUsersDao().all());
             }
         } else if (buttonPro != null) {
             if (confirm.equals("yes")) {
                 DaoFactory.getUsersDao().update(Long.valueOf(userId));
-                DaoFactory.getUsersDao().all();
+//                request.setAttribute("users", DaoFactory.getUsersDao().all());
             }
         }
         else {
             throw new ServletException("something went awry");
         }
-        DaoFactory.getUsersDao().all();
+        request.setAttribute("users", DaoFactory.getUsersDao().all());
         request.getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
     }
 }
