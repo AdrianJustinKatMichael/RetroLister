95436389aa37b452a2e878bc085ae39b0a86e0cb
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-04-13 11:09:32 -0500

    profile page wip

--- seeder.sql
+++ seeder.sql
@@ -10,6 +10,6 @@ VALUES (1, 'Final Fantasy Crystal Chronicles', 'Gamecube', 'gently used, dm for 
        (2, 'Marvel vs. Capcom: Clash of Super Heroes', 'PlayStation', 'open to trade or buy up to $80, dm with offers', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2s55.png', 'requesting'),
        (2, 'Sonic the Hedgehog', 'Sega Genesis', 'like new', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1zun.png', 'selling'),
        (3, 'Baldur''s Gate', 'Microsoft Windows', 'like new', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1s1j.png', 'selling'),
-       (3, 'Diablo II', 'Microsoft Windows', 'small scratches on disc but functional', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1s1j.png', 'selling'),
+       (3, 'Diablo II', 'Microsoft Windows', 'small scratches on disc but functional', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co3gfq.png', 'selling'),
        (4, 'Kirby Super Star', 'SNES', 'gently used $50', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co3fqp.png', 'selling'),
        (4, 'Pokémon Yellow', 'Game Boy', 'brand new unopened box', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2sa8.png', 'selling');
